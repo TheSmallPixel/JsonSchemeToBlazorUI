422fc8e8787f39d84cf84b1b9121f61d88cc6f51
Lorenzo Longaive <thesmallpixel@gmail.com>
2023-03-27 12:27:08 +0200

    first push

---
+++ JSBUI/JsonSchemaService.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSBUI
+{
+
+    public class JsonSchemaService
+    {
+        public JsonSchema LoadSchema(string jsonSchema)
+        {
+            return JsonConvert.DeserializeObject<JsonSchema>(jsonSchema);
+        }
+    }
+}
---
+++ JSBUI/JsonScheme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSBUI
+{
+
+    public class JsonSchema
+    {
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public List<string> Required { get; set; }
+        public Dictionary<string, JsonSchemaProperty> Properties { get; set; }
+    }
+
+    public class JsonSchemaProperty : JsonSchema
+    {
+        public string Description { get; set; }
+        public int? MinLength { get; set; }
+        public int? MaxLength { get; set; }
+        public List<string> Enum { get; set; }
+    }
+}
